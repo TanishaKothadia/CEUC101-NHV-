4d49d8e59160dbefe854b9045467073e05f388cf
TanishaKothadia <tanishakothadia@yahoo.com>
2025-08-19 22:50:58 +0530

    Add files via upload

---
+++ P3.C
@@ -0,0 +1,20 @@
+/*
+Programmer : Tanisha Kothadia
+Aim : Calculate BMI
+Date : 21-07-25
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float w,h,bmi;
+	clrscr();
+	printf("Enter your weight in kg: ");
+	scanf("%f",&w);
+	printf("Enter your height in meters: ");
+	scanf("%f",&h);
+	bmi=w/(h*h);
+	printf("Your calculated BMI is %f",bmi);
+	getch();
+
+}
