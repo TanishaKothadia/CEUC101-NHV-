0c74d148f78e17d9687de88efb72c71538a170d6
TanishaKothadia <tanishakothadia@yahoo.com>
2025-08-19 23:38:13 +0530

    Add files via upload

---
+++ P4.C
@@ -0,0 +1,22 @@
+/*
+Programmer : Tanisha Kothadia
+Aim : Datatypes and their sizes
+Date : 22-07-25
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i;
+	char c;
+	float f;
+	double d;
+	clrscr();
+	printf("The size of different datatypes is:");
+	printf("\nThe size of char is %d",sizeof(c));
+	printf("\nThe size of int is %d",sizeof(i));
+	printf("\nThe size of float is %d",sizeof(f));
+	printf("\nThe size of double is %d",sizeof(d));
+	getch();
+
+}
