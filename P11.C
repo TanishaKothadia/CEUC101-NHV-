8984fdddaa9da5ea6641e70e7bbb4515f8f28102
TanishaKothadia <tanishakothadia@yahoo.com>
2025-08-23 10:38:49 +0530

    Add files via upload

---
+++ P11.C
@@ -0,0 +1,16 @@
+/*
+Programmer : Tanisha Kothadia
+Aim : Grade
+Date : 25-07-25
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int m;
+	clrscr();
+	printf("Enter your marks: ");
+	scanf("%d",&m);
+	m>=90?printf("Grade A"):m>=80?printf("Grade B"):m>=70?printf("Grade C"):m>=60?printf("Grade D"):printf("Fail");
+	getch();
+}
