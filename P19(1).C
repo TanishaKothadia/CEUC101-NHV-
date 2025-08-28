88156225e0e707bf4f2eaeb9fef95aab262c0d86
TanishaKothadia <tanishakothadia@yahoo.com>
2025-08-28 13:10:34 +0530

    Add files via upload

---
+++ P19(1).C
@@ -0,0 +1,26 @@
+/*
+Programmer : Tanisha Kothadia
+Aim : Pattern
+Date : 25-08-25
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int c,r;
+	clrscr();
+	for(r=1;r<=5;r++)
+	{
+		for(c=1;c<=r;c++)
+		{
+			if(c%2==0)
+			{
+				printf("0");
+			}
+			else
+				printf("1");
+		}
+		printf("\n");
+	}
+	getch();
+}
