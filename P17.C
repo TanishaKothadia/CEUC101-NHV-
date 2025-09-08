1468acc0fc3bd53cc74e0ab3d005b13c8e59f623
TanishaKothadia <tanishakothadia@yahoo.com>
2025-09-08 23:36:55 +0530

    Add files via upload

---
+++ P17.C
@@ -0,0 +1,23 @@
+/*
+Programmer : Tanisha Kothadia
+Aim : Marathon
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int m=1;
+	float d=0.5;
+	clrscr();
+	while(1)
+	{
+		printf("\nMinute %d:Distance covered is %.2f",m,d);
+		m=m+1;
+		d=d+0.5;
+		if(d==10.0)
+		{
+			break;
+		}
+	}
+	getch();
+}
